5a0016113d855803c9ba4717414443a71566f4d4
Larisa Kosareva <kos.lv@yandex.com>
2020-11-23 23:33:36 +0100

    config setup

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 const express = require('express');
+
+const passport = require('passport');
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
 const app = express();// generates a new applications that represents a running express app
 
-app.get('/', (req, res) => {
-    res.send({hi: 'there'});
-})
+
+
+passport.use(new GoogleStrategy());
 
 app.listen(5000);
