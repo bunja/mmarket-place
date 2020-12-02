9dacc36d672ec8d5bf112607cb342331e80e7d55
Larisa Kosareva <kos.lv@yandex.com>
2020-12-02 14:25:10 +0100

    passport feractoring

--- services/passport.js
+++ services/passport.js
@@ -17,28 +17,25 @@ passport.deserializeUser((id, done) => {
     
 });
 
-passport.use(
-  new GoogleStrategy(
-    {
-      clientID: keys.googleClientID,
-      clientSecret: keys.googleClientSecret,
-      callbackURL: '/auth/google/callback'
-    },
-    (accessToken, refreshToken, profile, done) => {
-        User.findOne({googleId: profile.id })
-            .then((existingUser) => {
-                if(existingUser) {
-                    // we already have a record with the given profile id
-                    done(null, existingUser);
-                } else {
-                    // we don't have a  record with this ID, make a new record
-                    new User({ googleId: profile.id })//new model instance
-                        .save()
-                        .then(user => done(null, user));
-                }
-            })
-    
-        
+const oauthOptions = {
+    clientID: keys.googleClientID,
+    clientSecret: keys.googleClientSecret,
+    callbackURL: '/auth/google/callback',
+    proxy: true
+};
+
+const strategy = new GoogleStrategy(oauthOptions,
+    async (accessToken, refreshToken, profile, done) => {
+        const existingUser = await User.findOne({ googleId: profile.id});
+            
+        if (existingUser) {
+            // we already have a record with the given profile ID
+            return  done(null, existingUser);
+        } 
+        // we don't have a user record with this ID, make a new record
+        const user = await new User({ googleId: profile.id }).save();
+        done(null, user);
     }
-  )
 );
+
+passport.use(strategy);
