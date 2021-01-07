1246566d2de4c530d51d40727f13cf7212de26b7
Larisa Kosareva <kos.lv@yandex.com>
2021-01-07 01:31:39 +0100

    iambackbitches

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,9 +1,22 @@
 import React from 'react';
+import { BrowserRouter, Route} from 'react-router-dom';
+
+import Header from './Header';
+const DashBoard = () => <h2>DashBoard</h2>;
+const AdNew = () => <h2>AdNew</h2>;
+const Home = () => <h2>HOME</h2>;
 
 const App = () => {
     return (
-        <div>
-            Hi there!
+        <div className="container">
+            <BrowserRouter>
+                <div>
+                    <Header />
+                    <Route exact path="/" component={Home}/>
+                    <Route exact path="/myads" component={DashBoard}/>
+                    <Route path="/myads/new" component={AdNew}/>
+                </div>
+            </BrowserRouter>
         </div>
     );
 };
